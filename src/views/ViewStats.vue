ccacc46c00ed77eaa942878ebdc2b5fc0f1ededa
hoang227 <hoangg227@gmail.com>
2024-02-08 17:20:19 +0800

    add funcitonality to stats page

--- src/views/ViewStats.vue
+++ src/views/ViewStats.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="stats">
-    <h1>stats</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>stat</th>
+          <th>value</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>number of notes</td>
+          <td>{{ storeNotes.totalNotesCount }}</td>
+        </tr>
+        <tr>
+          <td>number of character (of all notes)</td>
+          <td>{{ storeNotes.totalCharactersCount }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+<script setup>
+/*
+  imports
+*/
+
+import { useStoreNotes } from '@/stores/storeNotes';
+
+/*
+  store
+*/
+
+const storeNotes = useStoreNotes()
+</script>
